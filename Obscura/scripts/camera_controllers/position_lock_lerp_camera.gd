0c3247b1336fd42d88dab819d677e04a567c0138
Keanu Chau <keanuchau@gmail.com>
2024-10-29 16:07:22 -0700

    Create PositionLockLerpCamera

---
+++ Obscura/scripts/camera_controllers/position_lock_lerp_camera.gd
@@ -0,0 +1,52 @@
+class_name PositionLockLerpCamera
+extends CameraControllerBase
+
+@export var follow_speed : float = 1.0
+@export var catchup_speed : float = 1.0
+@export var leash_distance : float = 1.0
+
+func _ready() -> void:
+	super()
+	position = target.position
+	
+func _process(delta: float) -> void:
+	if !current:
+		return
+	
+	# Show camera
+	if draw_camera_logic:
+		draw_logic()
+	
+	var target_position : Vector3 = target.global_position
+	var camera_position : Vector3 = global_position
+
+	super(delta)
+	
+func draw_logic() -> void:
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	# Horizontal line
+	immediate_mesh.surface_add_vertex(Vector3(-2.5, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(2.5, 0, 0))
+	# Vertical line
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, -2.5))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, 2.5))
+
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.WHITE
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
